e64aa57a8b84bc06dac771cb43cb449faf2e816f
Sasin Ransara <sasinransara@gmail.com>
2025-06-29 23:58:20 +0530

    initial commit

---
+++ eventbuzz/src/main/java/com/example/eventbuzz/EventbuzzApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventbuzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventbuzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventbuzzApplication.class, args);
+	}
+
+}
---
+++ eventbuzz/src/test/java/com/example/eventbuzz/EventbuzzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventbuzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventbuzzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
