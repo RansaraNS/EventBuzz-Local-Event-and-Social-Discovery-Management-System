da9dabbce11f60291f95cf68fefd966578cc4609
Sasin Ransara <sasinransara@gmail.com>
2025-07-02 18:41:04 +0530

    fix: resolve issue with changelog master file

--- eventbuzz/create-migration.bat
+++ eventbuzz/create-migration.bat
@@ -12,18 +12,20 @@ set minute=%datetime:~10,2%
 set second=%datetime:~12,2%
 set filename=%year%_%month%_%day%_%hour%%minute%%second%_%desc%.xml
 
-:: Define full file path
+:: Define file paths
 set filepath=src\main\resources\db\changelog\changelogs\%filename%
+set masterfile=src\main\resources\db\changelog\db.changelog.master.xml
+set tempfile=src\main\resources\db\changelog\temp_master.xml
 
 :: Create changelogs folder if not exist
 if not exist src\main\resources\db\changelog\changelogs (
     mkdir src\main\resources\db\changelog\changelogs
 )
 
-:: Tell user
+:: Tell user about file creation
 echo Creating %filename% ...
 
-:: Write XML content using a different approach
+:: Write XML content for the migration file
 (
 echo ^<?xml version="1.0" encoding="UTF-8"?^>
 echo ^<databaseChangeLog
@@ -37,6 +39,34 @@ echo.
 echo ^</databaseChangeLog^>
 ) > "%filepath%"
 
-:: Done message
 echo File created at: %filepath%
+
+:: Now update the master changelog file
+echo Adding entry to master changelog...
+
+:: Check if master file exists
+if not exist "%masterfile%" (
+    echo Error: Master changelog file not found at %masterfile%
+    pause
+    exit /b 1
+)
+
+:: Method 1: Using simple text replacement
+:: First, copy everything except the last line to temp file
+type "%masterfile%" | findstr /v "</databaseChangeLog>" > "%tempfile%"
+
+:: Add the new include line
+echo     ^<include file="changelogs/%filename%"/^> >> "%tempfile%"
+
+:: Add the closing tag back
+echo. >> "%tempfile%"
+echo ^</databaseChangeLog^> >> "%tempfile%"
+
+:: Replace the original file
+move "%tempfile%" "%masterfile%" > nul
+
+echo.
+echo ✓ Migration file created: %filename%
+echo ✓ Added to master changelog: db.changelog.master.xml
+echo.
 pause
